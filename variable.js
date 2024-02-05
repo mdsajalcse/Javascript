3b20898630c666fcf78f8aedc73721f61c63bafa
sajal <sajalcse2018@gmail.com>
2024-02-05 17:56:59 +0600

    variable types

--- variable.js
+++ variable.js
@@ -1,2 +1,20 @@
-whatIsJsVariables="containers for storing information";//string type value
+// JavaScript Variables can be declared in 4 ways:
+
+// 1. Automatically
+// 2. Using var
+// 3. Using let
+// 4. Using const
+
+var whatIsJsVariables="containers for storing information";//string type value
 console.log(whatIsJsVariables);
+//number
+var id=10;
+console.log(id+20);
+var isRight='True';
+console.log(isRight);
+//again number
+var firstValue=10;
+var secondValue=90;
+var totalValue=firstValue+secondValue;
+console.log(totalValue);
+
